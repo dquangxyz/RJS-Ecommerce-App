bebcd16b08b578523e38054b3c8b37cbfcce745d
dquangxyz <dq2209@gmail.com>
2022-12-25 20:23:59 +1100

    create a new section component to be used in Checkout and Cart page

---
+++ src/components/Layout/Section.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './Section.css'
+
+const Section = (props) => {
+  return (
+    <section className='py-5 bg-light'>
+      <div className='container'>
+          <div className='row px-4 px-lg-5 py-lg-4 align-items-center'>
+              <div className='col-lg-6'>
+                  <h1 className='h2 text-uppercase mb-0'>{props.title}</h1>
+              </div>
+              <div className='col-lg-6 text-lg-right'>
+                  <nav aria-label='breadcrumb'>
+                      <ol className='breadcrumb justify-content-lg-end mb-0 px-0'>
+                          <li className='breadcrumb-item active' aria-current='page'>{props.title}</li>
+                      </ol>
+                  </nav>
+              </div>
+          </div>
+      </div>
+    </section>
+  )
+}
+
+export default Section
