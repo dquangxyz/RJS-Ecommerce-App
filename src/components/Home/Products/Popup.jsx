58d3a42ca9202e6c6576beedf82efa39d7b74b94
dquangxyz <dq2209@gmail.com>
2022-12-29 17:56:02 +1100

    improve popup component

--- src/components/Home/Products/Popup.jsx
+++ src/components/Home/Products/Popup.jsx
@@ -3,6 +3,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { popupActions } from '../../../store/popup'
 
 import './Popup.css'
+
+const formatCurrency = (num) => {
+    return (+num).toLocaleString('vi')
+}
+
 const Popup = () => {
     const item = useSelector(state => state.popup.item) // return an object with keys: id, name, price, img, description
     const dispatch = useDispatch()
@@ -12,14 +17,20 @@ const Popup = () => {
             <div className='product-modal'>
                 <button id='btn-close' onClick={()=> dispatch(popupActions.hidePopup())}>X</button>
                 <div className='row'>
-                    <div className='col-lg-6'>
-                        <img className='product-image' src={item.img} alt='' />
+                    <div className='col-lg-7'>
+                        <img className='popup-product-image' src={item.img} alt='' />
                     </div>
-                    <div className='col-lg-6'>
-                        <div className='product-name'>{item.name}</div>
-                        <div className='product-price'>{item.price}</div>
-                        <div className='product-description'>{item.description}</div>
-                        <button className='btn-view-detail'>View Detail</button>
+                    <div className='col-lg-5'>
+                        <div className='popup-product-name'>{item.name}</div>
+                        <div className='popup-product-price'>{formatCurrency(item.price)} VND</div>
+                        <div className='popup-product-description'>{item.description}</div>
+                        <div id='btn-view-detail'>
+                            <a href={`/detail/${item.id}`} className='btn btn-dark' >
+                                <i className='fa fa-shopping-cart'></i>
+                                <span className='ml-2'>View Detail</span>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </div>
